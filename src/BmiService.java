835ace25489b01469cd963e9cce00b29b26d7aa9
Valentin Denisov <valentindenisov1989@gmail.com>
2022-10-31 15:27:49 +0400

    change BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double m, double h) {
-        double b = m / Math.pow(h, 2);
-        return b;
+    public double calculate(double bodyMass, double height) {
+        double bmi = bodyMass / Math.pow(height, 2);
+        return bmi;
     }
 }
